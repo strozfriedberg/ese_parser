841e5a4496af630e352f793e7ac6f71d2244dcd3
(OUT) Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2020-09-01 20:01:28 +0300

    use `clap` to build Config

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,49 @@
 use std::error::Error;
 use std::process;
-use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::Read;
 
+extern crate clap;
+use clap::{Arg, App};
+
 pub struct Config {
-    pub filename: String,
+    pub inp_file: String,
+    pub report_file: Option<&'static str>,
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
+    pub fn new() -> Result<Config, &'static str> {
+        let matches = App::new("ESE DB dump")
+            .version("0.1.0")
+            .arg(Arg::with_name("in")
+                .short("i")
+                .long("input")
+                .takes_value(true)
+                .required(true)
+                .help("Path to ESE db file"))
+            .arg(Arg::with_name("out")
+                .short("o")
+                .long("output")
+                .takes_value(true)
+                .help("Path to output report"))
+            .get_matches();
+
+        let inp_file = matches.value_of("in").unwrap();
+        println!(" inp_file: {}", inp_file);
+
+        let report_file = matches.value_of("out");
+        match report_file {
+            None => println!("No idea what your favorite number is."),
+            Some(s) => {
+                match s.parse::<i32>() {
+                    Ok(n) => println!("Your favorite number must be {}.", n + 5),
+                    Err(_) => println!("That's not a number! {}", s),
+                }
+            }
         }
 
-        let filename = args[1].clone();
-
-        Ok(Config { filename })
+        Ok(Config { inp_file : inp_file.to_string(), report_file : report_file.clone() })
     }
 }
 
@@ -31,7 +57,7 @@ fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = get_file_as_byte_vec(&config.filename);
+    let contents = get_file_as_byte_vec(&config.inp_file);
 
     println!("{:0x?}", &contents[..128]);
 
@@ -39,14 +65,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let config = Config::new().unwrap_or_else(|err| {  println!("Problem parsing arguments: {}", err);
+                                                                   process::exit(1);
+                                                                });
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-                                                                            println!("Problem parsing arguments: {}", err);
-                                                                            process::exit(1);
-                                                                        });
-
-    println!("file '{}'", config.filename);
+    println!("file '{}'", config.inp_file);
 
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
