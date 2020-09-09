31746a12d061533fccd755b56fc77271acbfc2dc
(OUT) Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2020-09-09 18:00:00 +0300

    [uint8_t; 8] instead of uint64_t, add unknown_val, use env_logger instead of println!, load backup_file_header

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #![allow(non_camel_case_types, non_upper_case_globals, clippy::mut_from_ref, clippy::cast_ptr_alignment)]
+#[macro_use] extern crate log;
+
+use env_logger;
 
 use std::process;
 
@@ -16,15 +19,15 @@ pub struct esedb_file_header {
     pub signature: uint32_t,
     pub format_version: uint32_t,
     pub file_type: uint32_t,
-    pub database_time: uint64_t,
+    pub database_time: [uint8_t; 8],
     pub database_signature: [uint8_t; 28],
     pub database_state: uint32_t,
-    pub consistent_postition: uint64_t,
-    pub consistent_time: uint64_t,
-    pub attach_time: uint64_t,
-    pub attach_postition: uint64_t,
-    pub detach_time: uint64_t,
-    pub detach_postition: uint64_t,
+    pub consistent_postition: [uint8_t; 8],
+    pub consistent_time: [uint8_t; 8],
+    pub attach_time: [uint8_t; 8],
+    pub attach_postition: [uint8_t; 8],
+    pub detach_time: [uint8_t; 8],
+    pub detach_postition: [uint8_t; 8],
     pub unknown1: uint32_t,
     pub log_signature: [uint8_t; 28],
     pub previous_full_backup: [uint8_t; 24],
@@ -39,11 +42,11 @@ pub struct esedb_file_header {
     pub format_revision: uint32_t,
     pub page_size: uint32_t,
     pub repair_count: uint32_t,
-    pub repair_time: uint64_t,
+    pub repair_time: [uint8_t; 8],
     pub unknown2: [uint8_t; 28],
-    pub scrub_database_time: uint64_t,
-    pub scrub_time: uint64_t,
-    pub required_log: uint64_t,
+    pub scrub_database_time: [uint8_t; 8],
+    pub scrub_time: [uint8_t; 8],
+    pub required_log: [uint8_t; 8],
     pub upgrade_exchange5_format: uint32_t,
     pub upgrade_free_pages: uint32_t,
     pub upgrade_space_map_pages: uint32_t,
@@ -53,13 +56,13 @@ pub struct esedb_file_header {
     pub unknown3: [uint8_t; 16],
     pub old_repair_count: uint32_t,
     pub ecc_fix_success_count: uint32_t,
-    pub ecc_fix_success_time: uint64_t,
+    pub ecc_fix_success_time: [uint8_t; 8],
     pub old_ecc_fix_success_count: uint32_t,
     pub ecc_fix_error_count: uint32_t,
-    pub ecc_fix_error_time: uint64_t,
+    pub ecc_fix_error_time: [uint8_t; 8],
     pub old_ecc_fix_error_count: uint32_t,
     pub bad_checksum_error_count: uint32_t,
-    pub bad_checksum_error_time: uint64_t,
+    pub bad_checksum_error_time: [uint8_t; 8],
     pub old_bad_checksum_error_count: uint32_t,
     pub committed_log: uint32_t,
     pub previous_shadow_volume_backup: [uint8_t; 24],
@@ -74,6 +77,7 @@ pub struct esedb_file_header {
     pub unknown5_4: [uint8_t; 32],
     pub unknown5_5: [uint8_t; 148 - 4 * 32],
     pub unknown_flags: uint32_t,
+    pub unknown_val: uint32_t,
 }
 
 use simple_error::SimpleError;
@@ -120,7 +124,7 @@ impl Config {
             .get_matches();
 
         let inp_file = matches.value_of("in").unwrap().to_owned();
-        println!(" inp_file: {}", inp_file);
+        debug!(" inp_file: {}", inp_file);
 
         let report_file = matches.value_of("out").to_owned();
         match report_file {
@@ -133,16 +137,17 @@ impl Config {
 }
 
 //https://stackoverflow.com/questions/38334994/how-to-read-a-c-struct-from-a-binary-file-in-rust
-use std::io::{self, BufReader, Read};
+use std::io::{self, BufReader, Read, Seek, SeekFrom};
 use std::fs::File;
 use std::path::Path;
 use std::mem::{self};
 use std::slice;
 
-fn read_struct<T, P: AsRef<Path>>(path: P) -> io::Result<T> {
+fn read_struct<T, P: AsRef<Path>>(path: P, file_offset: SeekFrom) -> io::Result<T> {
     let path = path.as_ref();
     let struct_size = mem::size_of::<T>();
     let mut reader = BufReader::new(File::open(path)?);
+    reader.seek(file_offset)?;
     let mut r : T = unsafe { mem::zeroed() };
     unsafe {
         let buffer = slice::from_raw_parts_mut(&mut r as *mut _ as *mut u8, struct_size);
@@ -152,11 +157,12 @@ fn read_struct<T, P: AsRef<Path>>(path: P) -> io::Result<T> {
 }
 
 pub fn run(config: Config) -> Result<(), EseParserError> {
-    let mut file_header = read_struct::<esedb_file_header, _>(config.inp_file).map_err(EseParserError::Io)?;
+    let mut db_file_header = read_struct::<esedb_file_header, _>(&config.inp_file, SeekFrom::Start(0))
+                                        .map_err(EseParserError::Io)?;
 
-    println!("{:0x?}", file_header);
+    debug!("db_file_header ({:X}): {:0X?}", mem::size_of::<esedb_file_header>(), db_file_header);
 
-    assert_eq!(file_header.signature, esedb_file_signature, "bad file_header.signature");
+    assert_eq!(db_file_header.signature, esedb_file_signature, "bad file_header.signature");
 
     fn calc_crc32(file_header: &&mut esedb_file_header) -> u32 {
         unsafe fn any_as_u8_slice<'a, T: Sized>(p: &'a &mut T) -> &'a mut [u8] {
@@ -180,20 +186,41 @@ pub fn run(config: Config) -> Result<(), EseParserError> {
         crc32
     }
 
-    let checksum = file_header.checksum;
-    assert_eq!(checksum, calc_crc32(&&mut file_header));
+    let stored_checksum = db_file_header.checksum;
+    let checksum = calc_crc32(&&mut db_file_header);
+    assert_eq!(stored_checksum, checksum, "wrong checksum");
+
+    let backup_file_header = read_struct::<esedb_file_header, _>(&config.inp_file, SeekFrom::Start(db_file_header.page_size as u64))
+        .map_err(EseParserError::Io)?;
+    if db_file_header.format_revision == 0 {
+        db_file_header.format_revision = backup_file_header.format_revision;
+    }
+    else if db_file_header.format_revision != backup_file_header.format_revision {
+        warn!("mismatch in format revision: {:#X} and backup: {:#X}", db_file_header.format_revision, backup_file_header.format_revision);
+    }
+    
+    if db_file_header.page_size == 0 {
+        db_file_header.page_size = backup_file_header.page_size;
+    }
+    else if db_file_header.page_size != backup_file_header.page_size {
+        warn!("mismatch in page size: {:#X} and backup: {:#X}", db_file_header.page_size, backup_file_header.page_size );
+        //db_file_header.page_size = (uint32_t) file_offset;
+    }
 
     Ok(())
 }
 
 fn main() {
-    let config = Config::new().unwrap_or_else(|err| {  println!("Problem parsing arguments: {}", err);
+    //env_logger::init();
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    let config = Config::new().unwrap_or_else(|err| {  error!("Problem parsing arguments: {}", err);
                                                                    process::exit(1);
                                                                 });
-    println!("file '{}'", config.inp_file);
+    info!("file '{}'", config.inp_file);
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        error!("Application error: {}", e);
 
         process::exit(1);
     }
