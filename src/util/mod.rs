0f71b3b7fe467e19ef549dac35c6314a2396c299
(OUT) Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2020-10-10 19:03:28 +0300

    fn any_as_u8_slice<...>(...)

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,10 @@
 pub mod config;
 pub mod dumper;
 pub mod reader;
+
+use std::mem;
+use std::slice;
+
+pub unsafe fn any_as_u8_slice<'a, T: Sized>(p: &'a &mut T) -> &'a mut [u8] {
+    slice::from_raw_parts_mut((*p as *const T) as *mut u8, mem::size_of::<T>())
+}
