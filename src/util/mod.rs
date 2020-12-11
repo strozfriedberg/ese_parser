d72c5ffbd247e3c09d5cc75a8ed143c4ef81dd00
(OUT) Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2020-12-11 20:33:38 +0200

    hide unused

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -8,7 +8,7 @@ use std::slice;
 /// # Safety
 ///
 /// use slice::from_raw_parts_mut
-pub unsafe fn any_as_u8_slice<'a, T: Sized>(p: &'a &mut T) -> &'a mut [u8] {
+pub unsafe fn _any_as_u8_slice<'a, T: Sized>(p: &'a &mut T) -> &'a mut [u8] {
     slice::from_raw_parts_mut((*p as *const T) as *mut u8, mem::size_of::<T>())
 }
 
