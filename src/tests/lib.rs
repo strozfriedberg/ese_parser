37b8de8c42c817f87c9e90df39b4e173eeb5d8ce
(OUT) Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2020-12-25 17:55:33 +0200

    struct Entourage

---
+++ src/tests/lib.rs
@@ -0,0 +1,33 @@
+//lib
+
+use std::path::PathBuf;
+
+use crate::util::config::Config;
+use crate::util::reader::load_db_file_header;
+use crate::ese::ese_db::FileHeader;
+
+pub(crate) const TEST_FILE: &str = r"data\DataStore.edb";
+
+pub(crate) struct Entourage {
+    pub(crate) config: Config,
+    pub(crate) db_file_header: FileHeader,
+}
+
+impl Entourage {
+    pub fn new() -> Entourage {
+        let _ = env_logger::try_init().or::<()>(Ok(()));
+
+        let config = match Config::new_for_file(&PathBuf::from(TEST_FILE), &""){
+            Ok(x) => x,
+            Err(e) => panic!("Could not create config: {}", e),
+        };
+
+        let db_file_header = match load_db_file_header(&config) {
+            Ok(x) => x,
+            Err(e) => panic!("Application error: {}", e),
+        };
+
+
+        Entourage{config: config, db_file_header: db_file_header}
+    }
+}
--- src/tests/settings.rs
+++
@@ -1,3 +0,0 @@
-//settings.rs
-
-pub(crate) const TEST_FILE: &str = r"data\DataStore.edb";
