e8199bb9d2016933d29d16647e2f725d9b639ac9
Oleg Dolgov <Oleg.Dolgov@gmail.com>
2021-01-15 03:08:33 -0800

    clang_args added to generate esent.rs using clang under Linux.

--- lib/build.rs
+++ lib/build.rs
@@ -5,14 +5,20 @@ use std::path::PathBuf;
 
 fn main() {
     println!(r"cargo:rustc-link-lib=esent");
-    println!(r"cargo:rustc-link-lib=Ole32");
-    println!(r"cargo:rustc-link-lib=OleAut32");
+    println!(r"cargo:rustc-link-lib=ole32");
+    println!(r"cargo:rustc-link-lib=oleaut32");
     println!(r"cargo:rustc-link-search=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0\um\x64");
 
     println!("cargo:rerun-if-changed=src/esent.h");
     let bindings = bindgen::Builder::default()
         .header("src/esent.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // required while cross-build from Linux
+        .clang_args(&[
+            "-D__int64=long long",
+            "-D_Pre_notnull_=",
+            "-D_Out_writes_bytes_opt_(a)=",
+            "-D_Out_cap_post_count_(a, b)="])
         .generate()
         .expect("Unable to generate bindings");
 
