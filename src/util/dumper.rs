015745a64589d09842a6a54422f509b8e07d53de
(OUT) Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2021-01-18 18:29:08 +0200

    up to date

--- src/util/dumper.rs
+++ src/util/dumper.rs
@@ -1,13 +1,13 @@
 //dumper.rs
 
 use prettytable::{Row, Cell};
-use comfy_table;
+//use comfy_table;
 extern crate hexdump;
 use std::string::ToString;
 
 use crate::ese::ese_db::{FileHeader, PageHeader};
 use crate::ese::{jet, ese_db};
-//use std::fmt::{self, /*Formatter,*/ Debug};
+//use std::fmt::{self, Debug};
 
 pub fn _dump_db_file_header(db_file_header: FileHeader) {
     let mut table = prettytable::Table::new();
