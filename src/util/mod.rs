84099768027f64b45a9c8cbdb3338da45de7c3d5
(OUT) Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2021-01-18 18:33:45 +0200

    generic _any_as_slice instead of _any_as_u8_slice/_any_as_u32_slice

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -8,15 +8,9 @@ use std::slice;
 /// # Safety
 ///
 /// use slice::from_raw_parts_mut
-pub unsafe fn _any_as_u8_slice<'a, T: Sized>(p: &'a &mut T) -> &'a mut [u8] {
-    slice::from_raw_parts_mut((*p as *const T) as *mut u8, mem::size_of::<T>())
-}
-
-/// # Safety
-///
-/// use slice::from_raw_parts_mut
-pub unsafe fn _any_as_u32_slice<'a, T: Sized>(p: &'a &mut T) -> &'a mut [u32] {
-    slice::from_raw_parts_mut((*p as *const T) as *mut u32, mem::size_of::<T>() / mem::size_of::<u32>())
+#[allow(clippy::mut_from_ref)]
+pub unsafe fn _any_as_slice<'a, U: Sized, T: Sized>(p: &'a &mut T) -> &'a mut [U] {
+    slice::from_raw_parts_mut((*p as *const T) as *mut U, mem::size_of::<T>() / mem::size_of::<U>())
 }
 
 #[macro_export]
