aa3c28782021cbca6fb43dcafb015f824f5b94e4
Oleg Dolgov <Oleg.Dolgov@gmail.com>
2021-04-01 03:00:43 -0700

    Linux build: run lib and python tests.

--- docker/build-wheel-linux.sh
+++ docker/build-wheel-linux.sh
@@ -16,9 +16,15 @@ docker run --rm \
 	-v ${PWD}:/ese_parser \
 	-v ${PWD}/builds:/builds \
 	$IMAGE_NAME /bin/bash -c '\
+	set -ex
+
 	cd ~
 	cp -r /ese_parser .
 	cd ese_parser
+
+	pushd lib
+	cargo test
+	popd
 
 	pushd app
 	cargo build --release
@@ -27,6 +33,11 @@ docker run --rm \
 
 	pushd python
 	maturin build --release
+
+	virtualenv test
+	./test/bin/pip install --force-reinstall target/wheels/ese_parser-0.1.0-cp36-abi3-manylinux2010_x86_64.whl
+	./test/bin/python py/test.py
+
 	cp target/wheels/ese_parser-0.1.0-cp36-abi3-manylinux2010_x86_64.whl /builds
 	popd
 '
