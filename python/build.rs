9a6e87de6f67c9fffc40e67f80da9badd100688b
(OUT) Oleg Dolgov <Oleg.Dolgov@gmail.com>
2021-05-27 15:31:05 +0300

    linux-version of main fn added.

--- python/build.rs
+++ python/build.rs
@@ -3,8 +3,10 @@ fn main() {
     println!(r"cargo:rustc-cdylib-link-arg=-undefined");
     println!(r"cargo:rustc-cdylib-link-arg=dynamic_lookup");
 }
-
 #[cfg(target_os = "windows")]
 fn main() {
     println!(r"cargo:rustc-link-search=../docker/Python37/libs");
+}
+#[cfg(target_os = "linux")]
+fn main() {
 }
