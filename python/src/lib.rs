c1c5e5d6c822f0e0dec1f8aa8e78ab98e237e444
lsukharnikov <lsukharnikov@strozfriedberg.com>
2021-12-16 11:21:40 -0500

    Getting rid of api, added _ match arm

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -325,6 +325,11 @@ impl PyEseDb {
                     Err(e) => return Err(PyErr::new::<exceptions::PyTypeError, _>(e.as_str().to_string()))
                 }
             },
+            _ => {
+                return Err(PyErr::new::<exceptions::PyTypeError, _>(
+                    format!("Unknown type {}, column: {}, id: {}, cbmax: {}, cp: {}",
+                        column.typ, column.name, column.id, column.cbmax, column.cp)))
+            },
             ESE_coltypDateTime => {
                 match self.jdb.get_column_date(table, column.id) {
                     Ok(ov) => {
