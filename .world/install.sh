a7823b0f96ecbdc1816be072c8ef2ae2189b6857
kimberly.stone <kimberly.stone@aon.com>
2022-03-09 12:21:53 -0800

    Windows build

--- .world/install.sh
+++ .world/install.sh
@@ -2,9 +2,15 @@
 
 . .world/build_config.sh
 
-if [[ "$Linkage" == 'static' || "$Target" != 'linux' ]]; then
+if [[ "$Linkage" == 'static' ]] || [[ "$Target" != 'linux' && "$Target" != 'windows_package' ]]; then
   exit
 fi
 
 mkdir -p $INSTALL/lib/python/ese_parser
 cp python/target/wheels/* $INSTALL/lib/python/ese_parser
+
+if [[ "$Target" == 'windows_package' ]]; then
+  pushd python
+  pip install . --use-feature=in-tree-build
+  popd
+fi
