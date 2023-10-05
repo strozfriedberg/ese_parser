44d5bac425a3730b992e87e1a240f68ce3f8ffd4
Shane McCulley <shane.mcculley@aon.com>
2023-10-05 03:42:36 +0000

    F!! Remove manual venv creation for build environment

--- .world/setup.sh
+++ .world/setup.sh
@@ -5,19 +5,3 @@
 if [[ "$Linkage" == 'static' || ( "$Target" == 'windows' ) ]]; then
   exit
 fi
-
-BASEDIR=$(pwd)
-
-VENV=venv
-if [[ "$Target" == 'windows_package' ]]; then
-  PYTHON=python
-  VENVBIN=Scripts
-else
-  PYTHON=python3
-  VENVBIN=bin
-fi
-
-$PYTHON -m venv --clear $VENV
-. "$VENV/$VENVBIN/activate"
-pip install toml maturin
-deactivate
