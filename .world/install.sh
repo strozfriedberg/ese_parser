9bfcdbdf76477f12979494ac8e71c8239c0d4b4f
Shane McCulley <shane.mcculley@aon.com>
2023-10-05 22:17:12 +0000

    F - Build wheel to default location then copy it

--- .world/install.sh
+++ .world/install.sh
@@ -7,7 +7,11 @@ if [[ "$Linkage" == 'static' || ( "$Target" == 'windows' ) ]]; then
 fi
 
 pushd python
-poetry run maturin build --release --interpreter python --no-sdist --out $INSTALL/lib/python/ese_parser
+poetry run maturin build --release --interpreter python --no-sdist
+
+mkdir -p $INSTALL/lib/python/ese_parser
+cp python/target/wheels/* $INSTALL/lib/python/ese_parser
+
 
 if [[ "$Target" == 'windows_package' ]]; then
   pip install .
